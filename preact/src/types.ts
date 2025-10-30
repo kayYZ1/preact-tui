077341cabffc268d6cfb9a8e408b72a438ba59d8
Kacper <overek7133@gmail.com>
2025-10-30 19:44:48 +0100

    feat: stricter types

--- preact/src/types.ts
+++ preact/src/types.ts
@@ -1,9 +1,32 @@
-export interface Instance {
-	type: "box" | "text";
-	props: any;
-	children: Instance[];
-	parent?: Instance;
-	yogaNode?: any;
+import Y from "yoga-layout";
+
+export interface BaseProps {
+	children?: any;
 }
 
-//ToDo: Add types for styling props
+export interface BoxProps extends BaseProps {
+	flex?: number | boolean;
+	flexDirection?: "row" | "column" | "row-reverse" | "column-reverse";
+	gap?: number;
+	padding?: number;
+	width?: number;
+	height?: number;
+	justifyContent?: "flex-start" | "center" | "flex-end" | "space-between" | "space-around" | "space-evenly";
+	alignItems?: "flex-start" | "center" | "flex-end" | "stretch" | "baseline";
+	flexWrap?: "wrap" | "wrap-reverse" | "nowrap";
+}
+
+export interface TextProps extends BaseProps {
+	color?: string;
+	bold?: boolean;
+	italic?: boolean;
+}
+
+export type ComponentProps = BoxProps | TextProps;
+
+export interface Instance {
+	type: "box" | "text";
+	props: ComponentProps;
+	children: Instance[];
+	yogaNode: ReturnType<typeof Y.Node.create>;
+}
